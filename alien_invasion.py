34830130408ce561e94551e7563c27ef12fe28fe
Kemalov Daniyar <kemalov2011@mail.ru>
2024-01-18 22:42:54 +0300

    Added more aliens in a row

--- alien_invasion.py
+++ alien_invasion.py
@@ -77,9 +77,19 @@ class AlienInvasion:
 
     def _create_fleet(self):
         """"Создание флота врагов(пришельцев)"""
-        # Создание пришельца
+        # Создание пришельца и вычисление их в ряду
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        numbers_aliens_x = available_space_x // (2 * alien_width)
+
+        # Создание первого ряда врагов
+        for alien_number in range(numbers_aliens_x):
+            alien = Alien(self)
+            alien.x = alien_width + 2 * alien_width * alien_number
+            alien.rect.x = alien.x
+            self.aliens.add(alien)
+
 
     def _update_screen_(self):
         """"Обновляет изображения на экране и отображает новый экран"""
