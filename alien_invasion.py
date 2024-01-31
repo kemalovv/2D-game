4cb0515dab81c2a84d30ec47c0f05f2fd9bdf9a2
Kemalov Daniyar <kemalov2011@mail.ru>
2024-01-31 23:33:42 +0300

    Create an alien fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -79,19 +79,26 @@ class AlienInvasion:
         """"Создание флота врагов(пришельцев)"""
         # Создание пришельца и вычисление их в ряду
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         available_space_x = self.settings.screen_width - (2 * alien_width)
         numbers_aliens_x = available_space_x // (2 * alien_width)
 
-        # Создание первого ряда врагов
-        for alien_number in range(numbers_aliens_x):
-            self._create_alien(alien_number)
+        # Количество врагов, появляющихся на экране
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height - (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
 
-    def _create_alien(self, alien_number):
+        # Создание флота вторжения
+        for row_number in range(number_rows):
+            for alien_number in range(numbers_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         alien.x = alien_width + 2 * alien_width * alien_number
         alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
     def _update_screen_(self):
